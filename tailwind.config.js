7f5f3d98c468281b2ac4b29d2c99e37bcd64b517
Hamed Ghaderi <hamediran1182gh@gmail.com>
2024-07-26 20:51:22 +0330

    feat: create homepage component
    create header and hero section and style them

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "outfit": "outfit"
+      },
+      width: {
+        "21r": "21rem"
+      },
+      maxWidth: {
+        "whole": "1440px",
+      },
+      colors: {
+        "back": "#f9f9f9",
+        "gray": "#000000b3"
+      }
+    },
   },
   plugins: [],
 };
