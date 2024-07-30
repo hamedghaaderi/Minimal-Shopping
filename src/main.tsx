dfd9a297d9aac418e3645974f7fb846ba32a4448
sajjad <sajjad1907dehghani@gmail.com>
2024-07-30 23:09:40 +0330

    feat: create a hook for get products
    first, create query client in main.tsx and then create hook

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,25 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 6000,
+      refetchInterval: false,
+      refetchOnMount: false,
+      refetchOnReconnect: false,
+      refetchOnWindowFocus: false,
+      gcTime: Infinity,
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </>
 );
