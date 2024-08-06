e1da527977f08c69516d880b5b9731f68ea76485
sajjad <sajjad1907dehghani@gmail.com>
2024-08-06 23:24:33 +0330

    feat: create total discount for calculate discount in store

--- src/store/basket.ts
+++ src/store/basket.ts
@@ -4,6 +4,7 @@ const useBasket = create((set, get: () => any) => ({
   products: [],
   invoice: {
     totalPrice: 0,
+    totalDiscount: 0,
   },
   action: {
     add: (product: any) => {
@@ -14,6 +15,8 @@ const useBasket = create((set, get: () => any) => ({
         return set((oldBasket: any) => ({
           invoice: {
             totalPrice: oldBasket.invoice.totalPrice + product.price,
+            totalDiscount:
+              (oldBasket.invoice.totalPrice + product.price) * 0.075,
           },
           products: oldBasket.products.map((_product: any) => {
             if (_product.id === product.id) {
@@ -30,17 +33,22 @@ const useBasket = create((set, get: () => any) => ({
       set((oldBasket: any) => ({
         invoice: {
           totalPrice: oldBasket.invoice.totalPrice + product.price,
+          totalDiscount:
+          (oldBasket.invoice.totalPrice + product.price) * 0.075,
         },
         products: [...oldBasket.products, { ...product, quantity: 1 }],
       }));
     },
     remove: (product: any) => {
       const shouldRemove = get().products.some(
-        (_product: any) => _product.quantity === 1 && _product.id === product.id)
+        (_product: any) => _product.quantity === 1 && _product.id === product.id
+      );
       if (shouldRemove) {
         return set((oldBasket: any) => ({
           invoice: {
             totalPrice: oldBasket.invoice.totalPrice - product.price,
+            totalDiscount:
+            (oldBasket.invoice.totalPrice - product.price) * 0.075,
           },
           products: oldBasket.products.filter(
             (_product: any) => _product.id !== product.id
@@ -51,6 +59,8 @@ const useBasket = create((set, get: () => any) => ({
       set((oldBasket: any) => ({
         invoice: {
           totalPrice: oldBasket.invoice.totalPrice - product.price,
+          totalDiscount:
+          (oldBasket.invoice.totalPrice - product.price) * 0.075,
         },
         products: oldBasket.products.map((_product: any) => {
           if (_product.id === product.id) {
